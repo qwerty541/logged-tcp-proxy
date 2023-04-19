7de07ade69ff1f6f35ec7654cb2b07502ef7bfbb
Alexandr <qwerty541zxc@gmail.com>
2023-04-19 16:53:28 +0300

    Accept required arguments from command line instead of built-in constants

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bytes::BytesMut;
+use clap::Parser;
 use logged_stream::ConsoleLogger;
 use logged_stream::DefaultFilter;
 use logged_stream::HexDecimalFormatter;
@@ -12,25 +13,32 @@ use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net as tokio_net;
 use tokio::time::timeout;
 
-lazy_static::lazy_static! {
-    static ref LISTEN_ADDR: net::SocketAddr = net::SocketAddr::new(
-        net::IpAddr::V4(net::Ipv4Addr::new(0, 0, 0, 0)), 20502
-    );
-    static ref CONNECT_TO_ADDR: net::SocketAddr = net::SocketAddr::new(
-        net::IpAddr::V4(net::Ipv4Addr::new(127, 0, 0, 1)), 20582
-    );
+#[derive(Debug, Clone, Parser)]
+#[command(next_line_help = true)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    /// Logging level of read/write operations.
+    #[arg(short, long, default_value = "debug")]
+    level: String,
+    /// Address on which TCP listener should be binded.
+    #[arg(short, long)]
+    bind_listener_addr: net::SocketAddr,
+    /// Address of remote server.
+    #[arg(short, long)]
+    remote_addr: net::SocketAddr,
+    /// Incoming connection reading timeout.
+    #[arg(short, long, default_value = "60")]
+    timeout: u64,
 }
 
-const SOURCE_STREAM_READ_TIMEOUT_SECS: u64 = 60;
-
-async fn incoming_connection_handle(source_stream: tokio_net::TcpStream) {
+async fn incoming_connection_handle(arguments: Arguments, source_stream: tokio_net::TcpStream) {
     let (mut source_stream_read_half, mut source_stream_write_half) = io::split(LoggedStream::new(
         source_stream,
         HexDecimalFormatter::new(None),
         DefaultFilter::default(),
         ConsoleLogger::new_unchecked("debug"),
     ));
-    let destination_stream = tokio_net::TcpStream::connect(*CONNECT_TO_ADDR)
+    let destination_stream = tokio_net::TcpStream::connect(arguments.remote_addr)
         .await
         .expect("Failed to connect to destination address");
     let (mut destination_stream_read_half, mut destination_stream_write_half) =
@@ -65,7 +73,7 @@ async fn incoming_connection_handle(source_stream: tokio_net::TcpStream) {
         let mut buffer = BytesMut::with_capacity(2048);
         'source_stream_handle: loop {
             let Ok(Ok(read_length)) = timeout(
-                Duration::from_secs(SOURCE_STREAM_READ_TIMEOUT_SECS),
+                Duration::from_secs(arguments.timeout),
                 source_stream_read_half.read_buf(&mut buffer)
             ).await else {
                 destination_stream_handle.abort();
@@ -86,21 +94,24 @@ async fn incoming_connection_handle(source_stream: tokio_net::TcpStream) {
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-    env::set_var("RUST_LOG", "debug");
+    let arguments = Arguments::parse();
+
+    env::set_var("RUST_LOG", arguments.level.clone());
     env_logger::init();
 
-    let listener = tokio_net::TcpListener::bind(*LISTEN_ADDR)
+    let listener = tokio_net::TcpListener::bind(arguments.bind_listener_addr)
         .await
         .expect("Failed to bind tcp listener");
 
-    log::debug!("Listener binded, waiting for incoming connections...");
+    log::info!("Listener binded, waiting for incoming connections...");
 
     loop {
+        let cloned_arguments = arguments.clone();
         let accept = listener.accept().await;
         match accept {
             Ok((stream, addr)) => {
-                log::debug!("Incoming connection from {addr}");
-                tokio::spawn(incoming_connection_handle(stream));
+                log::info!("Incoming connection from {addr}");
+                tokio::spawn(incoming_connection_handle(cloned_arguments, stream));
             }
             Err(e) => {
                 log::error!("Failed to accept incoming connection due to {e}");
